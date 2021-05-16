64ef8bd2a34ded16e416a18376785ab2c07764cc
ak1484 <ankitsaharan1484@gmail.com>
2021-05-16 10:32:48 +0530

    Let keyword(let vs var)

--- ECMAscript.js
+++ ECMAscript.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+//Let keyword(let vs var)
+var a=10
+if(true)
+{
+    var a=20
+    console.log(a)
+}
+console.log(a)
+var a=10
+if(true)
+{
+    let a=20
+    console.log(a)
+}
+console.log(a)
+// let a=20
+// let a=10
+// console.log(a)
+//error
+//hosting can't use with let but it can use with var
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+for (var i = 0; i < 10; i++) {
+    setTimeout(() => console.log(i), 1000);
+  }
