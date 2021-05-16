7eee84ce644e78723f0d48f659deb9a6aac67680
ak1484 <ankitsaharan1484@gmail.com>
2021-05-16 11:18:55 +0530

    Arrow function

--- ECMAscript.js
+++ ECMAscript.js
@@ -1,19 +1,31 @@
-//find and findIndex
-let a=[10,20,7,50,1,100]
-console.log(a.find((i)=>i>20))
-console.log(a.find((i)=>i>20 && i<100))
-console.log(a.findIndex((i)=>i>20))
-console.log(a.findIndex((i)=>i>20 && i<100))
-let data=[
-    {id:1,name:'a'},
-    {id:5,name:'b'},
-    {id:3,name:'c'},
-    {id:9,name:'d'},
-    {id:2,name:'e'},
-]
-function costum(i)
-{
-    return i.id>3;
-}
-console.log(data.find(costum))
-console.log(data.findIndex(costum))
+//Arrow function
+let a = [20, 4, 5, 12, 6];
+// function test1()
+// {return console.log("test1")}
+// //normal
+// test1()
+// let test2=function()
+// {return console.log("test2")}
+// //anonymus
+// test2()
+// test3=()=>
+// {return console.log("test3")}
+// //arrow fun
+// test3()
+// let newa=a.map((i)=>i*2)
+// console.log(newa)
+let item = {
+  name: "ankit",
+  getName: function () {
+    console.log("normal", this);
+  },
+  getNameArrow:()=> {
+    console.log("arrow", this);
+  },
+};
+//khud ka this call kra
+//this.getName.bind(this) for parent this
+item.getName();
+//parent ka this call krta hai tabhi
+//()=>this.test() ye apne parent ka he this uthata hai
+item.getNameArrow();
