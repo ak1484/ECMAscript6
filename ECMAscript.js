7002367669ac77a173f97dcf7800786ebdb30ea3
ak1484 <ankitsaharan1484@gmail.com>
2021-05-17 13:24:27 +0530

    all done

--- ECMAscript.js
+++ ECMAscript.js
@@ -1,6 +1,1 @@
-//Exponentiation operator
-console.log(2**3)//power
-console.log(2*3)//multi
-console.log(10**-2)//neg pow
-console.log(10**NaN)
-console.log(2**3**2)//multi pow
+//done all module and generator video se dekh lo biro
