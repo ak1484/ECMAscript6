16309969fde1b358e6cdc0c179806b4b985d47e6
ak1484 <ankitsaharan1484@gmail.com>
2021-05-17 12:20:26 +0530

    New global function

--- ECMAscript.js
+++ ECMAscript.js
@@ -1,17 +1,8 @@
-//Promise in es6
-let data=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject("some issues")
-    },2000)
-})
-data.then((item)=>{
-    console.log(item)
-}).catch((err)=>{
-    console.log("catch block",err)
-})
-// let data1=fetch('url')
-// data1.then((result)=>{
-//     return item.json()
-// }).then((resp)=>{
-//     console.log("output",resp)
-// })
+//New global function
+console.log(isFinite(10))
+console.log(isFinite(10/0))
+console.log(isFinite(null))//isme true krega
+console.log(isNaN(10))
+console.log(isNaN("10"))
+console.log(isNaN("10ank"))
+console.log(isNaN(null))//isme bhi number he smjga
